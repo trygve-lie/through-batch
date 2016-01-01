c9c3ebc143c864f2db5e629c0339c16c2d089938
Trygve Lie <github@trygve-lie.com>
2016-01-01 02:43:33 +0100

    Added transform function

--- test/batch.js
+++ test/batch.js
@@ -56,3 +56,57 @@ tap.test('source array with 10 objects, batching size is 4 - result should write
         }));
 
 });
+
+
+
+tap.test('source array with 10 objects, batching size is 4 - result should write 3 arrays', (t) => {
+
+    var count = 0;
+
+    sourceStream(sourceArray)
+        .pipe(batch(4))
+        .pipe(new stream.Writable({
+            objectMode : true,
+            write: function(chunk, encoding, next) {
+
+                if (count === 2) {
+                    t.equal(chunk.length, 2);
+                    t.end();
+                    return;
+                } 
+
+                t.equal(chunk.length, 4);
+                count++;
+                next()
+            }
+        }));
+
+});
+
+
+
+tap.test('transform function appended - result should be transformed', (t) => {
+
+    var count = 0;
+
+    sourceStream(sourceArray)
+        .pipe(batch(1, (obj) => {
+            return {uuid : obj.id};
+        }))
+        .pipe(new stream.Writable({
+            objectMode : true,
+            write: function(chunk, encoding, next) {
+
+                t.similar(chunk, [{uuid : sourceArray[count].id}]);
+
+                if (count === 9) {
+                    t.end();
+                    return;
+                } 
+
+                count++;
+                next()
+            }
+        }));
+
+});
